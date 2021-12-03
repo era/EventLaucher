1ceed893df1598af8d6261187f9a3138650294ef
era <the@communist.dev>
2021-12-03 07:29:02 +0000

    avoiding panic

--- src/rule.rs
+++ src/rule.rs
@@ -7,8 +7,8 @@ pub enum Op {
     Eq,
     NotEq,
     GreaterThan,
-    SmallerThan
-} 
+    SmallerThan,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Rule {
@@ -19,9 +19,9 @@ pub struct Rule {
 
 impl Rule {
     pub fn parse(rule: &str) -> Result<Rule, &'static str> {
-        let re = Regex::new(r"((\.{0,1}[a-z0-9\[\]]{1,}){0,}) (eq|!eq|>|<) ([0-9a-z ]{1,})")
-                        .unwrap();
-        
+        let re =
+            Regex::new(r"((\.{0,1}[a-z0-9\[\]]{1,}){0,}) (eq|!eq|>|<) ([0-9a-z ]{1,})").unwrap();
+
         let groups = re.captures(rule).unwrap();
 
         let op = match &groups[3] {
@@ -29,9 +29,9 @@ impl Rule {
             "!eq" => Op::NotEq,
             ">" => Op::GreaterThan,
             "<" => Op::SmallerThan,
-            _ => panic!("Not valid Operation")
+            _ => return Err("Not valid Operation"),
         };
-        
+
         let parsed_rule = Rule {
             json_element: groups[1].to_string(),
             op: op,
@@ -43,22 +43,21 @@ impl Rule {
         };
     }
 
-
-    // arg, this is terrible, forcing the convertion 
+    // arg, this is terrible, forcing the convertion
     // just to double check > and < are used correctly
     // arg, terrible, terrible
     fn validate_rule(rule: &Rule) -> Result<i32, ParseIntError> {
         return match rule.op {
             Op::SmallerThan => rule.value.parse::<i32>(),
             Op::GreaterThan => rule.value.parse::<i32>(),
-            _ => Ok(0)
+            _ => Ok(0),
         };
     }
 
     pub fn should_exec(self: &Self, event: &str) -> bool {
         let mut event_value = str::replace(&self.walk_json(event).unwrap(), "\"", "");
         event_value = event_value.trim().to_string();
-        
+
         return match self.op {
             Op::Eq => event_value == self.value,
             Op::NotEq => event_value != self.value,
@@ -67,14 +66,11 @@ impl Rule {
     }
 
     fn check_op_numeric_condition(self: &Self, value: &str) -> bool {
-
         if let Ok(numeric_value) = value.parse::<i32>() {
-
             return match self.op {
                 Op::SmallerThan => numeric_value < self.value.parse::<i32>().unwrap(),
                 Op::GreaterThan => numeric_value > self.value.parse::<i32>().unwrap(),
-                _ => false
-
+                _ => false,
             };
         } else {
             return false;
@@ -84,19 +80,22 @@ impl Rule {
     fn walk_json(self: &Self, json: &str) -> Result<String, jq_rs::Error> {
         return jq_rs::run(&self.json_element, json);
     }
-
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::rule::Rule;
     use crate::rule::Op;
-    
+    use crate::rule::Rule;
+
     #[test]
     fn parse_rule() {
         let r = Rule::parse(".event.type eq success").unwrap();
-        let expected = Rule { json_element: ".event.type".to_string(), op: Op::Eq, value: "success".to_string() };
-        
+        let expected = Rule {
+            json_element: ".event.type".to_string(),
+            op: Op::Eq,
+            value: "success".to_string(),
+        };
+
         assert_eq!(expected, r);
     }
 
@@ -123,3 +122,4 @@ mod tests {
         assert!(r.should_exec(json));
     }
 }
+
